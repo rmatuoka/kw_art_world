b47b36905115f8bf328f18838e2d82e434396084
Loco <rmatuoka@gmail.com>
2015-08-25 16:29:52 -0300

    fix category migration

--- db/migrate/20140604191348_create_categories.rb
+++ db/migrate/20140604191348_create_categories.rb
@@ -2,7 +2,6 @@ class CreateCategories < ActiveRecord::Migration
   def self.up
     create_table :categories do |t|
       t.string :title
-      t.string :subtitle
       t.text :description
       t.boolean :active,:null => false, :default => 1 
       t.boolean :published,:null => false, :default => 1 
