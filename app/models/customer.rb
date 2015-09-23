c70266d4446496edf9f8676242146c3df9586d2a
Rafael Matuoka Chiocchetti <rmatuoka@sato7.com.br>
2015-09-23 15:41:37 -0300

    inserecao da conversao de imagem de cliente para pb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,10 @@
 class Customer < ActiveRecord::Base
-  attr_accessible :title, :active, :published
+  attr_accessible :title, :active, :published, :image
 
   has_attached_file :image, :styles =>{
-                                     :thumb=>"93x93>"
-                                      }
+                                     :thumb=>"93x93>",
+                                     :front => "198x105>",
+                                     :front_pb => "198x105>"
+                                      },
+                                      :convert_options => { :front_pb => '-colorspace gray'}
 end
